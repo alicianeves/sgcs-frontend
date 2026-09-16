8f85242a9a98da837caaa471e38402c424a57c53
alicianeves <aliciacostta9@gmail.com>
2026-09-16 15:23:18 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Hello Word</p>
+      <button onClick={() => setCount(count + 1)}>
+        Count {count}
+      </button>
+    </>
+  )
+}
+
+export default App
